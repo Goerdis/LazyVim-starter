10d12e72f8051f47691d3d804dde387767da4996
Thomas_J <43466586+Goerdis@users.noreply.github.com>
2026-08-13 13:23:12 +0200

    Update keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set('n', '<C-A-PageUp>', ':bn<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<C-A-PageDown>', ':bp<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-A-PageUp>', ':bp<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-A-PageDown>', ':bn<CR>', { noremap = true, silent = true })
